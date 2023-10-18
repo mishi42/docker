eb18c174160f6d5fd058c09de11ba44825913509
mishi42 <140249303+mishi42@users.noreply.github.com>
2023-10-19 01:03:11 +0900

    Create jupyter_notebook_config.py

---
+++ jupyter/JupyterHub/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+
+c.ServerApp.notebook_dir = "/mnt"
+c.ServerApp.root_dir = "/mnt"
+c.Spawner.notebook_dir = '/mnt'
+c.Spawner.root_dir = '/mnt'
+#c.Spawner.default_url = '/lab' 
+c.Authenticator.whitelist = {'admin'}
+c.Authenticator.admin_users = {'admin'}
